d34bdba717ebbc2483910226496955b31ca3055d
Le4anndr0 <ricardosk8532@gmail.com>
2023-05-13 00:16:36 -0300

    Se agrega el proyecto

---
+++ ejercicio2.js
@@ -0,0 +1,12 @@
+const listaConElementos = {
+    nombre: "Ricardo Leandro Avila",
+    edad: 19,
+    esDesarrollador: false,
+    fechaDeNacimiento: new Date("december 18 2003"),
+    libroFavorito: {
+        titulo: "Flores para algernon",
+        autor: "Daniel Keyes",
+        fecha: new Date("april 01 1959"),
+        url: new URL('https://es.wikipedia.org/wiki/Flores_para_Algernon')
+    },
+}
